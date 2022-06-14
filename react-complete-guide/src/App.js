14da7347002c121b0f7b37cbf1d8db868657cfd8
EliasMura <elias-murat@outlook.com>
2022-06-14 11:34:41 -0300

    initial commit

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -0,0 +1,7 @@
+function ExpenseItem() {
+  return (
+    <h2>Teste</h2>
+  );
+}
+
+export default ExpenseItem;
