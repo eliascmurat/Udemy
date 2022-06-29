33ec4f3444c729819f371edfd498749db3c0cfd3
EliasMura <elias-murat@outlook.com>
2022-06-29 14:51:13 -0300

    RestTemplate

--- UdemySpring/NelioAlves/ms-course/hr-worker/src/main/resources/data.sql
+++ UdemySpring/NelioAlves/ms-course/hr-worker/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_worker (name, daily_Income) VALUES ('Yusuke', 200.0);
-INSERT INTO tb_worker (name, daily_Income) VALUES ('Gon', 300.0);
-INSERT INTO tb_worker (name, daily_Income) VALUES ('Luffy', 250.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Yusuke', 250.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Gon', 200.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Luffy', 300.0);
