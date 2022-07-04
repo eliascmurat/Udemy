f2647c8d0ad585e2279c1b1f1aaf29d500421a2d
EliasMura <elias-murat@outlook.com>
2022-07-04 14:15:12 -0300

    Hystrix fault tolerance

--- UdemySpring/NelioAlves/ms-course/hr-payroll/src/main/java/com/devsuperior/hrpayroll/resources/PaymentResource.java
+++ UdemySpring/NelioAlves/ms-course/hr-payroll/src/main/java/com/devsuperior/hrpayroll/resources/PaymentResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping(value = "/payments")
@@ -17,6 +18,7 @@ public class PaymentResource {
     @Autowired
     private PaymentService paymentService;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(
         @PathVariable Long workerId, 
@@ -25,5 +27,11 @@ public class PaymentResource {
         Payment payment = paymentService.getPayment(workerId, days);
         return ResponseEntity.ok(payment);
     }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+        Payment payment = new Payment("Test", 400.0, days);
+        return ResponseEntity.ok(payment);
+    }
+
 
 }
