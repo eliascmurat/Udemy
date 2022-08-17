fb2e6d2ee5480f09a7958c491c3de9154429abae
EliasMura <elias-murat@outlook.com>
2022-08-17 11:16:48 -0300

    criando metodo placePiece na classe Board

--- UdemyJava/NelioAlves/xadrez/src/main/java/br/com/islink/boardgame/Board.java
+++ UdemyJava/NelioAlves/xadrez/src/main/java/br/com/islink/boardgame/Board.java
@@ -34,4 +34,9 @@ public class Board {
     public Piece getPiece(Position position) {
         return pieces[position.getRow()][position.getColumn()];
     }
+
+    public void placePiece(Piece piece, Position position) {
+        pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;
+    }
 }
