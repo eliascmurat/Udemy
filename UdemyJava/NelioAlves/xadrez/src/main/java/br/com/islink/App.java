b19a646e1f9eea25cf3a3a60d84a05656ebdfff7
EliasMura <elias-murat@outlook.com>
2022-08-17 10:53:11 -0300

    criando classe Position

--- UdemyJava/NelioAlves/xadrez/src/main/java/br/com/islink/App.java
+++ UdemyJava/NelioAlves/xadrez/src/main/java/br/com/islink/App.java
@@ -1,7 +1,11 @@
 package br.com.islink;
 
+import br.com.islink.boardgame.Position;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Position pos = new Position(3, 3);
+        System.out.println(pos);
     }
 }
---
+++ UdemyJava/NelioAlves/xadrez/src/main/java/br/com/islink/boardgame/Position.java
@@ -0,0 +1,32 @@
+package br.com.islink.boardgame;
+
+public class Position {
+    private int row;
+    private int column;
+    
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return "Position {column: " + column + ", row: " + row + "}";
+    }
+}
