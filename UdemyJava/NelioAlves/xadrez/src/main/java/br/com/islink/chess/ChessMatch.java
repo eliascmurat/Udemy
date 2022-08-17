632c60d006a2363c23590835dac855db41cdeb04
EliasMura <elias-murat@outlook.com>
2022-08-17 11:24:51 -0300

    criando metodo de configuracoes iniciais na classe ChessMatch

--- UdemyJava/NelioAlves/xadrez/src/main/java/br/com/islink/chess/ChessMatch.java
+++ UdemyJava/NelioAlves/xadrez/src/main/java/br/com/islink/chess/ChessMatch.java
@@ -1,12 +1,16 @@
 package br.com.islink.chess;
 
 import br.com.islink.boardgame.Board;
+import br.com.islink.boardgame.Position;
+import br.com.islink.chess.pieces.King;
+import br.com.islink.chess.pieces.Rook;
 
 public class ChessMatch {
     private Board board;
 
     public ChessMatch() {
         board = new Board(8, 8);
+        initialSetup();
     }
 
     public ChessPiece[][] getPieces() {
@@ -17,5 +21,11 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+
+    private void initialSetup() {
+        board.placePiece(new Rook(board, Color.BLACK), new Position(0, 0));
+        board.placePiece(new Rook(board, Color.BLACK), new Position(0, 7));
+        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
     }
 }
