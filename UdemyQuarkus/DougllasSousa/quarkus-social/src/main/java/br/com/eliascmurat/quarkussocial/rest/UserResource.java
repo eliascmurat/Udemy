2d2cc9b46cca14a67dbb8fff096c6f2c55cc14a6
EliasMura <elias-murat@outlook.com>
2022-11-04 13:26:32 -0300

    criando delete e update

--- UdemyQuarkus/DougllasSousa/quarkus-social/src/main/java/br/com/eliascmurat/quarkussocial/rest/UserResource.java
+++ UdemyQuarkus/DougllasSousa/quarkus-social/src/main/java/br/com/eliascmurat/quarkussocial/rest/UserResource.java
@@ -2,12 +2,16 @@ package br.com.eliascmurat.quarkussocial.rest;
 
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import br.com.eliascmurat.quarkussocial.domain.model.User;
 import br.com.eliascmurat.quarkussocial.rest.dto.CreateUserRequest;
@@ -27,12 +31,43 @@ public class UserResource {
 
     @POST
     @Transactional
-    public Response createUser(CreateUserRequest createUserRequest) {
+    public Response createUser(CreateUserRequest userRequest) {
         User user = new User();
-        user.setAge(createUserRequest.getAge());
-        user.setName(createUserRequest.getName());
+        user.setAge(userRequest.getAge());
+        user.setName(userRequest.getName());
         user.persist();
 
         return Response.ok(user).build();
+    }
+
+    @DELETE
+    @Path("{userId}")
+    @Transactional
+    public Response deleteUser(@PathParam("userId") Long userId) {
+        User user = User.findById(userId);
+
+        if (user == null) {
+            return Response.status(Status.NOT_FOUND).build();
+        }
+
+        user.delete();
+
+        return Response.ok().build();
+    }
+
+    @PUT
+    @Path("{userId}")
+    @Transactional
+    public Response updateUser(@PathParam("userId") Long userId, CreateUserRequest userRequest) {
+        User user = User.findById(userId);
+
+        if (user == null) {
+            return Response.status(Status.NOT_FOUND).build();
+        }
+
+        user.setAge(userRequest.getAge());
+        user.setName(userRequest.getName());
+
+        return Response.ok().build();
     }
 }
