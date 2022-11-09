7d21d2ee0b766b375bea9ae482e4ae1b8b59d11c
EliasMura <elias-murat@outlook.com>
2022-11-09 15:38:37 -0300

    melhorando codigo com base no SonarQube

--- UdemyQuarkus/DougllasSousa/quarkus-social/src/main/java/br/com/eliascmurat/quarkussocial/domain/repository/FollowerRepository.java
+++ UdemyQuarkus/DougllasSousa/quarkus-social/src/main/java/br/com/eliascmurat/quarkussocial/domain/repository/FollowerRepository.java
@@ -2,7 +2,6 @@ package br.com.eliascmurat.quarkussocial.domain.repository;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 import javax.enterprise.context.ApplicationScoped;
 
