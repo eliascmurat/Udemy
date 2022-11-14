d8cc0603496ec0e00157e597cd3d44898617f5bc
EliasMura <elias-murat@outlook.com>
2022-11-14 09:54:31 -0300

    Melhorando CreayePostRequest test - by Sonar

--- UdemyQuarkus/DougllasSousa/quarkus-social/src/main/java/br/com/eliascmurat/quarkussocial/rest/dto/CreatePostRequest.java
+++ UdemyQuarkus/DougllasSousa/quarkus-social/src/main/java/br/com/eliascmurat/quarkussocial/rest/dto/CreatePostRequest.java
@@ -2,9 +2,11 @@ package br.com.eliascmurat.quarkussocial.rest.dto;
 
 import javax.validation.constraints.NotBlank;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class CreatePostRequest {
     @NotBlank(message = "Text is required")
     private String text;
