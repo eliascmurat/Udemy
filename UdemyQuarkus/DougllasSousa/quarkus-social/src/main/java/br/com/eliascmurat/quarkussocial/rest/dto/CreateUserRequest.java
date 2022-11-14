4532c1cdda65ce4d1db5a76da0fcce474b79944e
EliasMura <elias-murat@outlook.com>
2022-11-14 09:38:19 -0300

    Add @Positive

--- UdemyQuarkus/DougllasSousa/quarkus-social/src/main/java/br/com/eliascmurat/quarkussocial/rest/dto/CreateUserRequest.java
+++ UdemyQuarkus/DougllasSousa/quarkus-social/src/main/java/br/com/eliascmurat/quarkussocial/rest/dto/CreateUserRequest.java
@@ -2,14 +2,20 @@ package br.com.eliascmurat.quarkussocial.rest.dto;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class CreateUserRequest {
     @NotBlank(message = "Name is required")
     private String name;
     
     @NotNull(message = "Age is required")
+    @Positive(message = "Age must be a positive number")
     private Integer age;
 }
--- UdemyQuarkus/DougllasSousa/quarkus-social/src/main/java/br/com/eliascmurat/quarkussocial/rest/dto/FollowerRequest.java
+++ UdemyQuarkus/DougllasSousa/quarkus-social/src/main/java/br/com/eliascmurat/quarkussocial/rest/dto/FollowerRequest.java
@@ -1,8 +1,11 @@
 package br.com.eliascmurat.quarkussocial.rest.dto;
 
+import javax.validation.constraints.Positive;
+
 import lombok.Data;
 
 @Data
 public class FollowerRequest {
+    @Positive(message = "FollowerId must be a positive number")
     private Long followerId;
 }
